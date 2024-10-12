64393c297b7f28fb118ab3233e5d140afdef291a
ahmed arafa <70607269+Ahmed14112017@users.noreply.github.com>
2024-10-13 02:37:35 +0300

    navbar

--- src/Shared/Component/Navbar/Navbar.tsx
+++ src/Shared/Component/Navbar/Navbar.tsx
@@ -1,8 +1,28 @@
+import { useContext } from "react"
+import { authcontext } from "../../../Context/Authcontext"
 
 export default function Navbar() {
+  const {userdata}=useContext(authcontext)
+  console.log(userdata)
   return (
-    <div className="border-b-4 border-slate-400 w-full h-10">
-      navbar
+    <div className="border-b-4 border-1 border-gray-200 w-full h-20 flex flex-col justify-center p-3 ">
+      <div className="flex justify-between items-center h-20">
+        <div className="flex justify-between items-center w-1/2 border-e-2 h-20 pr-2">
+          <h3 className=" border-e-2 h-full flex justify-center items-center p-2">Group</h3>
+          <button className="py-3 px-5 border-2 rounded-3xl font-bold"><i className="fa-solid fa-circle-plus pr-2"></i>New quiz</button>
+          </div>
+          <div className="flex justify-between items-center w-1/2 pl-2">
+          <div className=" flex justify-between items-center">
+          <i className="fa-solid fa-envelope px-10  border-e-2 "></i>
+          <i className="fa-solid fa-bell  px-10  border-e-2"></i>
+          </div>
+         <div className="pl-2">
+         <div>{userdata.email}</div>
+         <div>{userdata.role}</div>
+         </div>
+          
+        </div>
+      </div>
     </div>
   )
 }
